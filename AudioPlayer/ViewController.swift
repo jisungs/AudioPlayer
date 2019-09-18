a3ba03d15f3731ae3c0dbb235e079a30ac784d6b
jisung <jisung.smart@gmail.com>
2019-09-18 16:35:09 +0900

    Add button and control music by slider

--- AudioPlayer/ViewController.swift
+++ AudioPlayer/ViewController.swift
@@ -12,10 +12,39 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player = AVAudioPlayer()
-
+    
+    
+    @IBOutlet weak var sliderOutlet: UISlider!
+    
+    @IBAction func play(_ sender: Any) {
+        
+        player.play()
+    }
+    
+    
+    @IBAction func pause(_ sender: Any) {
+        
+        player.pause()
+    }
+    
+    @IBAction func slider(_ sender: Any) {
+        
+        player.volume = sliderOutlet.value
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let audioPath = Bundle.main.path(forResource: "sheep", ofType: "mp3")
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+        } catch {
+            
+        }
     }
 
 
