dd882390effb89aad2293af9a35c9382098dbd7a
jisung <jisung.smart@gmail.com>
2019-09-23 15:41:09 +0900

    Add progress bar and for that I can controller music files status

--- AudioPlayer/ViewController.swift
+++ AudioPlayer/ViewController.swift
@@ -12,19 +12,32 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player = AVAudioPlayer()
+     let audioPath = Bundle.main.path(forResource: "sheep", ofType: "mp3")
     
+    var timer = Timer()
+    
+    @objc func updateProgressBar(){
+        
+        progressSlider.value = Float(player.currentTime)
+        
+    }
     
     @IBOutlet weak var sliderOutlet: UISlider!
+    @IBOutlet weak var progressSlider: UISlider!
     
     @IBAction func play(_ sender: Any) {
         
         player.play()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateProgressBar), userInfo: nil, repeats: true)
     }
     
     
     @IBAction func pause(_ sender: Any) {
         
         player.pause()
+        
+        timer.invalidate()
     }
     
     @IBAction func slider(_ sender: Any) {
@@ -32,16 +45,46 @@ class ViewController: UIViewController {
         player.volume = sliderOutlet.value
     }
     
+    @IBAction func progress(_ sender: Any) {
+        
+        player.currentTime = TimeInterval(progressSlider.value)
+        
+       // progressSlider.value = player.
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        
+        progressSlider.value = 0
+        
+        timer.invalidate()
+        
+        player.pause()
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+            //progressSlider.maximumValue = Float(player.duration)
+            
+        } catch {
+            
+        }
+        
+        
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let audioPath = Bundle.main.path(forResource: "sheep", ofType: "mp3")
-        
         do {
             
             try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+            progressSlider.maximumValue = Float(player.duration)
+            
         } catch {
             
         }
